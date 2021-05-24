9cc0695ab1cbbba20ac4742b8dafe0e9f711f9df
Mikita Abramowicz <gentled.druzhinnik@gmail.com>
2021-05-24 23:05:59 +0300

    feat: implement exercise-4

--- Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
@@ -1,5 +1,45 @@
 #import "LevelOrderTraversal.h"
 
 NSArray *LevelOrderTraversalForTree(NSArray *tree) {
-    return nil;
+    
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    
+    NSMutableArray *arrayWithIndexes = [[NSMutableArray alloc] init];
+    
+    int count = 0;
+    NSNumber *numberValue;
+    
+    if (tree.count == 0 || [tree[0] isKindOfClass:[NSNull class]]) {
+        return @[];
+    }
+    
+    for (int i = 0; i < tree.count; i++) {
+        
+        if (![tree[i] isKindOfClass:[NSNull class]]) {
+            
+            numberValue = [NSNumber numberWithInt:count];
+            
+            if (![arrayWithIndexes containsObject:numberValue]) {
+                [arrayWithIndexes addObject:numberValue];
+                [result addObject: [NSMutableArray new]];
+            }
+            
+            [result[count] addObject:tree[i]];
+            count++;
+        }
+        
+        if ([tree[i] isKindOfClass:[NSNull class]] && [tree[i+1] isKindOfClass:[NSNull class]]) {
+            count--;
+            i++;
+        }
+        
+    }
+    
+    for (int i = 0; i < result.count; i++) {
+        if ([result[i] count] == 0) {
+            [result removeObject:result[i]];
+        }
+    }
+    
+    return result;
 }
